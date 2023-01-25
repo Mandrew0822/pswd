cd23544967dd2b1091d21bcac865e89c88b510b9
Mandrew0822 <110215582+Mandrew0822@users.noreply.github.com>
2023-01-25 21:12:11 +0000

    updated to install dependant packages

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Compile the C program
-gcc pswd.c -o pswd
+# Check if OpenSSL development library is installed
+if ! [ -x "$(command -v libssl-dev)" ]; then
+    echo "OpenSSL development library is not installed. Installing..."
+    # Install OpenSSL development library
+    apt-get update
+    apt-get install -y libssl-dev
+else
+    echo "OpenSSL development library is already installed."
+fi
 
-# Move the executable to /bin
-sudo mv pswd /bin
+# Compile the C program with the OpenSSL library
+gcc -o pswd pswd.c -I/usr/include/openssl -lssl -lcrypto
+
+# Move the executable to a directory in the system PATH
+mv pswd /usr/local/bin/
+
+echo "pswd has been installed and can be run by typing 'pswd' in the terminal."
